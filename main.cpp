dbf81138ddab1a6ca3b367fc81a4f8b0dcee56e4
secwang <monadic.io@gmail.com>
2018-01-28 23:13:54 +0800

    add catch.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <cpr/cpr.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    auto r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
+                      cpr::Authentication{"user", "pass"},
+                      cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+    r.status_code;                  // 200
+    r.header["content-type"];       // application/json; charset=utf-8
+    r.text;                         // JSON text string
+
+    std::cout << r.status_code << r.header["content-type"] << r.text << std::endl;
+
+
 }
